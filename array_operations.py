e62af1de23f3e9379bfd05062cdeb49da200c90b
Bebirli Feride <bferide0808@gmail.com>
2026-08-04 23:58:51 +0400

    Add array_operations.py

---
+++ array_operations.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+sira1 = np.array([1, 2, 3])
+sira2 = np.array([4, 5, 6])
+
+print("Addition:")
+print(sira1 + sira2)
+
+print("\nSubtraction:")
+print(sira1 - sira2)
+
+print("\nMultiplication:")
+print(sira1 * sira2)
+
+print("\nDivision:")
+print(sira2 / sira1)
