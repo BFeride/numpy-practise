702edacc37e3edc73f5800f369584608cde1a4fd
Bebirli Feride <bferide0808@gmail.com>
2026-08-05 00:19:04 +0400

    Add slicing.py

---
+++ slicing.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+numbers = np.array([10, 20, 30, 40, 50, 60])
+
+print("First Three:")
+print(numbers[:3])
+
+print("\nLast Three:")
+print(numbers[3:])
+
+print("\nMiddle Elements:")
+print(numbers[2:5])
