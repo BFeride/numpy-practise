78f1317dc6039f1c856cb2fe0d4e3f4d3107717e
Bebirli Feride <bferide0808@gmail.com>
2026-08-04 23:54:27 +0400

    Add arrays.py

---
+++ arrays.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+numbers = np.array([10, 20, 30, 40, 50])
+
+print("Array:")
+print(numbers)
+
+print("\nData Type:")
+print(numbers.dtype)
+
+print("\nShape:")
+print(numbers.shape)
+
+print("\nSize:")
+print(numbers.size)
