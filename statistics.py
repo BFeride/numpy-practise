1436a4bc8d09254ded780c7fa1952ca0ba36e8e2
Bebirli Feride <bferide0808@gmail.com>
2026-08-05 00:28:37 +0400

    Add statistics.py

---
+++ statistics.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+scores=np.arraya([78, 85, 90, 92, 88])
+
+print(f"Scores {scores}")
+
+print(f"Mean: {np.mean(scores)}")
+print(f"Median: {np.median(scores)}")
+print(f"Maximum: {np.max(scores)}")
+print(f"Minimum: {np.min(scores)}")
+print(f"Standard Deviation: {np.std(scores)}")
